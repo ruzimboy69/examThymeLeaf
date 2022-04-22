bebc47178267ca5f3a172ca66dcfb95ff6c9c0c0
@RoyKholms <ruzimboykholmurotov@gmail.com>
2022-04-22 10:56:14 +0500

    git commit -m "qoshildi"

--- src/main/java/com/example/demo/Mvc/HomePageController.java
+++ src/main/java/com/example/demo/Mvc/HomePageController.java
@@ -4,6 +4,7 @@ import com.example.demo.payload.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +13,10 @@ public class HomePageController {
     @GetMapping
     public String getHomePage(Model model ){
         model.addAttribute("userDto",new UserDto());
+        return "registration";
+    }
+    @PostMapping
+    public String addAll(Model model){
         return "registration";
     }
 }
